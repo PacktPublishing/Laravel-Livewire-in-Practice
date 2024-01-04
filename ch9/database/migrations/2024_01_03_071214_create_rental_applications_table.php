696fa3df9d678240467da6c5179e916940879ba6
Simon Angatia <simonnabuko@gmail.com>
2024-01-04 05:06:13 +0300

    ch8

---
+++ ch9/database/migrations/2024_01_03_071214_create_rental_applications_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rental_applications', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('car_id');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('status')->default('pending');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('car_id')->references('id')->on('cars');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rental_applications');
+    }
+};
